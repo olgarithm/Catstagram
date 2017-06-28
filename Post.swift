0c1bfcfb467189227da85fcb2e437d0417893b1d
Olga Andreeva <olgarithm@fb.com>
2017-06-28 11:24:34 -0700

    Added login/logout functionality and picking photos to upload

---
+++ Post.swift
@@ -0,0 +1,13 @@
+//
+//  Post.swift
+//  Catstagram
+//
+//  Created by Olga Andreeva on 6/27/17.
+//  Copyright © 2017 Olga Andreeva. All rights reserved.
+//
+
+import UIKit
+
+class Post: NSObject {
+
+}
